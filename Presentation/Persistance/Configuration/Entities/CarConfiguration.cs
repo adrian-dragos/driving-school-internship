845bf7f16117ec599ea9f2edea086e498562f756
adrian-dragos <adrian123795@gmail.com>
2021-12-07 20:35:52 +0200

    Add Database Seeding for Cars and Instructors

---
+++ Presentation/Persistance/Configuration/Entities/CarConfiguration.cs
@@ -0,0 +1,83 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Configuration.Entities
+{
+    public class CarConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            builder.ToTable("Cars");
+            builder.HasData(
+                new Car
+                {
+                    Id = 1,
+                    CarModelType = CarModelType.DaciaLogan,
+                    CarGear = CarGear.Manual,
+                    CarFabricationTime = new DateTime(
+                        Convert.ToInt32(2015),
+                        Convert.ToInt32(08),
+                        1),
+                    RegistrationNumber = "TM 152",
+                    IsAvailable = false,
+                },
+                new Car
+                {
+                    Id = 2,
+                    CarModelType = CarModelType.DaciaSandero,
+                    CarGear = CarGear.Manual,
+                    CarFabricationTime = new DateTime(
+                        Convert.ToInt32(2017),
+                        Convert.ToInt32(02),
+                        1),
+                    RegistrationNumber = "TM 824",
+                    IsAvailable = false,
+                },
+                new Car
+                {
+                    Id = 3,
+                    CarModelType = CarModelType.SkodaFabia,
+                    CarGear = CarGear.Manual,
+                    CarFabricationTime = new DateTime(
+                        Convert.ToInt32(2016),
+                        Convert.ToInt32(02),
+                        1),
+                    RegistrationNumber = "TM 046",
+                    IsAvailable = true,
+                },
+                new Car
+                {
+                    Id = 4,
+                    CarModelType = CarModelType.SkodaRoomster,
+                    CarGear = CarGear.Automatic,
+                    CarFabricationTime = new DateTime(
+                        Convert.ToInt32(2017),
+                        Convert.ToInt32(03),
+                        1),
+                    RegistrationNumber = "TM 055",
+                    IsAvailable = false,
+                },
+                new Car
+                {
+                    Id = 5,
+                    CarModelType = CarModelType.RenaultZoe,
+                    CarGear = CarGear.Electric,
+                    CarFabricationTime = new DateTime(
+                        Convert.ToInt32(2020),
+                        Convert.ToInt32(02),
+                        1),
+                    RegistrationNumber = "AR 552",
+                    IsAvailable = false,
+                }
+                ); ;
+
+        }
+    }
+}
--- Presentation/Persistance/Configuration/Entities/InstructorConfiguration.cs
+++ Presentation/Persistance/Configuration/Entities/InstructorConfiguration.cs
@@ -14,6 +14,63 @@ namespace Persistance.Configuration.Entities
         public void Configure(EntityTypeBuilder<Instructor> builder)
         {
             builder.ToTable("Instructors");
+            builder.HasData(
+                    new Instructor
+                    {
+                        Id = 1,
+                        FirstName = $"Mihai",
+                        LastName = $"Ionascu",
+                        Email = $"mihai.ionascu23@gmail.com",
+                        PhoneNumber = $"+40 742 950 144",
+                        Birthday = new DateTime(1982, 02, 27),
+                        IsCurrentlyEmployed = true,
+                        CarId = 2,
+                    },
+                    new Instructor
+                    {
+                        Id = 2,
+                        FirstName = $"Cristian",
+                        LastName = $"Ceboatari",
+                        Email = $"cristian.ceb@gmail.com",
+                        PhoneNumber = $"+40 715 675 614",
+                        Birthday = new DateTime(1992, 12, 25),
+                        IsCurrentlyEmployed = false,
+                        CarId = null,
+                    },
+                    new Instructor
+                    {
+                        Id = 3,
+                        FirstName = $"Radu",
+                        LastName = $"Mazur",
+                        Email = $"radu.mazur88@gmail.com",
+                        PhoneNumber = $"+40 722 101 021",
+                        Birthday = new DateTime(1988, 08, 17),
+                        IsCurrentlyEmployed = true,
+                        CarId = 5,
+                    },
+                    new Instructor
+                    {
+                        Id = 4,
+                        FirstName = $"Dionis",
+                        LastName = $"Agapii",
+                        Email = $"dionis.agapii@gmail.com",
+                        PhoneNumber = $"+40 751 551 100",
+                        Birthday = new DateTime(1978, 11, 01),
+                        IsCurrentlyEmployed = true,
+                        CarId = 1,
+                    },
+                    new Instructor
+                    {
+                        Id = 5,
+                        FirstName = $"Denis",
+                        LastName = $"Codur",
+                        Email = $"condur.denis515@gmail.com",
+                        PhoneNumber = $"+40 712 229 545",
+                        Birthday = new DateTime(1996, 05, 15),
+                        IsCurrentlyEmployed = true,
+                        CarId = 3,
+                    }
+                );
         }
     }
 }
