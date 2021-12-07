d6d5cfc129f0f256c7a1e598db6b6ed146c7d2dd
adrian-dragos <adrian123795@gmail.com>
2021-12-07 17:29:26 +0200

    Add Cars and Students Controller

---
+++ Presentation/PresentationApi/Controllers/CarsController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CarsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+    }
+}
---
+++ Presentation/PresentationApi/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
