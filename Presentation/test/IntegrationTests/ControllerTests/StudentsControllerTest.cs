5e662c3dc0cd230060ed1429c507e0caa4fd7dd6
adrian-dragos <adrian123795@gmail.com>
2021-12-11 13:52:48 +0200

    Add Intergration Tests for StudentsController

--- Presentation/test/IntegrationTests/ControllerTests/StudentsControllerTest.cs
+++ Presentation/test/IntegrationTests/ControllerTests/StudentsControllerTest.cs
@@ -45,9 +45,9 @@ namespace IntegrationTests.ControllerTests
             var response = await client.GetAsync("api/students");
 
             var result = await response.Content.ReadAsStringAsync();
-            var books = JsonConvert.DeserializeObject<List<StudentDto>>(result);
+            var students = JsonConvert.DeserializeObject<List<StudentDto>>(result);
 
-            var book = books.FirstOrDefault(b => b.Id == 6);
+            var student = students.FirstOrDefault(b => b.Id == 6);
         }
 
         [TestMethod]
@@ -57,64 +57,59 @@ namespace IntegrationTests.ControllerTests
             var response = await client.GetAsync("api/students/7");
 
             var result = await response.Content.ReadAsStringAsync();
-            var book = JsonConvert.DeserializeObject<StudentDto>(result);
+            var student = JsonConvert.DeserializeObject<StudentDto>(result);
 
-            StudentAssert(book);
-        }
-
-        [TestMethod]
-        public async Task Post_Book_ShouldReturnCreatedResponse()
-        {
-            var book = new CreateStudentCommand()
-            {
-                StudentDto = new CreateStudentDto()
-                {
-                    FirstName = "Ion",
-                    LastName = "Petru",
-                    Email = "ion.petru@gmail.com",
-                    PhoneNumber = "+40 040 061 142",
-                    Birthday = new DateTime(1930, 11, 12)
-                }
-            };
-
-            var client = _factory.CreateClient();
-            var response = await client.PostAsync("/api/students",
-                new StringContent(JsonConvert.SerializeObject(book), Encoding.UTF8, "application/json"));
-
-            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
-        }
-
-
-        //[TestMethod]
-        //public async Task Post_Book_ShouldReturnCreatedBook()
-        //{
-        //    var newBook = new CreateStudentCommand
-        //    {
-        //        StudentDto = new CreateStudentDto()
-        //        {
-        //            FirstName = "Ion",
-        //            LastName = "Petru",
-        //            Email = "ion.petru@gmail.com",
-        //            PhoneNumber = "+40 040 061 142",
-        //            Birthday = new DateTime(1930, 11, 12)
-        //        }
-        //    };
-
-        //    var client = _factory.CreateClient();
-        //    var response = await client.PostAsync("/api/students",
-        //        new StringContent(JsonConvert.SerializeObject(newBook), Encoding.UTF8, "application/json"));
-
-        //    var result = await response.Content.ReadAsStringAsync();
-        //    var book = JsonConvert.DeserializeObject<int>(result);
-        //}
-
-        private static void StudentAssert(StudentDto student)
-        {
             Assert.AreEqual("Marin", student.FirstName);
             Assert.AreEqual("Grosu", student.LastName);
             Assert.AreEqual("grosu.marin41@gmail.com", student.Email);
             Assert.AreEqual("+40 614 411 421", student.PhoneNumber);
             Assert.AreEqual(new DateTime(2003, 04, 18), student.Birthday);
+        }
+
+        [TestMethod]
+        public async Task PostStudent_ShouldReturnCreatedResponse()
+        {
+            var newStudent = new CreateStudentDto()
+            {
+                FirstName = "Ion",
+                LastName = "Petru",
+                Email = "ion.petru@gmail.com",
+                PhoneNumber = "+40 040 061 142",
+                Birthday = new DateTime(1930, 11, 12)
+            };
+
+            var client = _factory.CreateClient();
+            var response = await client.PostAsync("/api/students",
+                new StringContent(JsonConvert.SerializeObject(newStudent), Encoding.UTF8, "application/json"));
+
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.Created);
+        }
+
+
+        [TestMethod]
+        public async Task PostStudent_ShouldReturnCreatedStudent()
+        {
+            var newStudent = new CreateStudentDto()
+            {
+                FirstName = "Ion",
+                LastName = "Petru",
+                Email = "ion.petru@gmail.com",
+                PhoneNumber = "+40 040 061 142",
+                Birthday = new DateTime(1930, 11, 12)
+            };
+
+            var client = _factory.CreateClient();
+            var response = await client.PostAsync("/api/students",
+                new StringContent(JsonConvert.SerializeObject(newStudent), Encoding.UTF8, "application/json"));
+
+            var result = await response.Content.ReadAsStringAsync();
+            var student = JsonConvert.DeserializeObject<StudentDto>(result);
+
+            Assert.AreEqual(newStudent.FirstName, student.FirstName);
+            Assert.AreEqual(newStudent.LastName, student.LastName);
+            Assert.AreEqual(newStudent.PhoneNumber, student.PhoneNumber);
+            Assert.AreEqual(newStudent.Email, student.Email);
+            Assert.AreEqual(newStudent.Birthday, student.Birthday);
         }
     }
 }
