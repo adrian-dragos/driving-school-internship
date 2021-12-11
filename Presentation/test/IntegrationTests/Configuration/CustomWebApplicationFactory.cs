77d6c7eda19b1982ff34920c24aa3d5e26b62d59
adrian-dragos <adrian123795@gmail.com>
2021-12-11 09:57:11 +0200

    Add Intergration Test For Studets

---
+++ Presentation/test/IntegrationTests/Configuration/CustomWebApplicationFactory.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistance;
+using System;
+using System.Linq;
+
+namespace IntegrationTests.Configuration
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        private SqliteConnection _connection;
+
+        public CustomWebApplicationFactory()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var serviceDescriptor = services.SingleOrDefault(d =>
+                    d.ServiceType ==
+                        typeof(DbContextOptions<ApplicationContext>));
+
+                services.Remove(serviceDescriptor);
+
+
+
+                // Create a new service provider.
+                var serviceProvider = new ServiceCollection()
+                        .AddEntityFrameworkSqlite()
+                        .BuildServiceProvider();
+
+
+                services.AddDbContextFactory<ApplicationContext>(options =>
+                    {
+                        options.UseSqlite(_connection);
+                        options.UseInternalServiceProvider(serviceProvider);
+                    }
+                    );
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+
+                    var db = scopedServices.GetRequiredService<ApplicationContext>();
+
+                    db.Database.EnsureDeleted();
+
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _connection.Close();
+            _connection.Dispose();
+        }
+    }
+}
---
+++ Presentation/test/IntegrationTests/ControllerTests/StudentsControllerTest.cs
@@ -0,0 +1,120 @@
+﻿using Application.DTOs.EntityDtos.Person.Student;
+using Application.Features.Students.Commands.CreateStudent;
+using IntegrationTests.Configuration;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using PresentationApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.ControllerTests
+{
+    [TestClass]
+    public class StudentsControllerTest
+    {
+        private static TestContext _testContext;
+        private static WebApplicationFactory<Startup> _factory;
+
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            _testContext = testContext;
+            _factory = new CustomWebApplicationFactory<Startup>();
+        }
+
+        [TestMethod]
+        public async Task GetStudents_ShouldReturnOkResponse()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("api/students");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task GetStudents_ShouldReturnExistingStudent()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("api/students");
+
+            var result = await response.Content.ReadAsStringAsync();
+            var books = JsonConvert.DeserializeObject<List<StudentDto>>(result);
+
+            var book = books.FirstOrDefault(b => b.Id == 6);
+        }
+
+        [TestMethod]
+        public async Task GetStudentsById_ShouldReturnExistingStudent()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("api/students/7");
+
+            var result = await response.Content.ReadAsStringAsync();
+            var book = JsonConvert.DeserializeObject<StudentDto>(result);
+
+            StudentAssert(book);
+        }
+
+        [TestMethod]
+        public async Task Post_Book_ShouldReturnCreatedResponse()
+        {
+            var book = new CreateStudentCommand()
+            {
+                StudentDto = new CreateStudentDto()
+                {
+                    FirstName = "Ion",
+                    LastName = "Petru",
+                    Email = "ion.petru@gmail.com",
+                    PhoneNumber = "+40 040 061 142",
+                    Birthday = new DateTime(1930, 11, 12)
+                }
+            };
+
+            var client = _factory.CreateClient();
+            var response = await client.PostAsync("/api/students",
+                new StringContent(JsonConvert.SerializeObject(book), Encoding.UTF8, "application/json"));
+
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+        }
+
+
+        //[TestMethod]
+        //public async Task Post_Book_ShouldReturnCreatedBook()
+        //{
+        //    var newBook = new CreateStudentCommand
+        //    {
+        //        StudentDto = new CreateStudentDto()
+        //        {
+        //            FirstName = "Ion",
+        //            LastName = "Petru",
+        //            Email = "ion.petru@gmail.com",
+        //            PhoneNumber = "+40 040 061 142",
+        //            Birthday = new DateTime(1930, 11, 12)
+        //        }
+        //    };
+
+        //    var client = _factory.CreateClient();
+        //    var response = await client.PostAsync("/api/students",
+        //        new StringContent(JsonConvert.SerializeObject(newBook), Encoding.UTF8, "application/json"));
+
+        //    var result = await response.Content.ReadAsStringAsync();
+        //    var book = JsonConvert.DeserializeObject<int>(result);
+        //}
+
+        private static void StudentAssert(StudentDto student)
+        {
+            Assert.AreEqual("Marin", student.FirstName);
+            Assert.AreEqual("Grosu", student.LastName);
+            Assert.AreEqual("grosu.marin41@gmail.com", student.Email);
+            Assert.AreEqual("+40 614 411 421", student.PhoneNumber);
+            Assert.AreEqual(new DateTime(2003, 04, 18), student.Birthday);
+        }
+    }
+}
